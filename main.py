Fixed grad computation

main.py
@@ -1,14 +1,19 @@
 import torch
-
+import numpy as np
 from vit.configs import ViT_B16_Config
 from vit.heads import MLPHead
 from vit.vit import ViT
 
 if __name__ == "__main__":
     embed_dim = ViT_B16_Config['embed_dim']
-    head = MLPHead(embed_dim, 10)
+    torch.autograd.set_detect_anomaly(True)
+    head = MLPHead(embed_dim, 1)
     vit = ViT(image_size=32, head=head, **ViT_B16_Config)
 
     inpt = torch.rand((1, 3, 32, 32))
-    print(vit(inpt))
+
+    ls = torch.nn.CrossEntropyLoss(reduction='mean')
+    res = vit(inpt)
+    loss = ls(res, torch.from_numpy(np.array([[0.]])))
+    loss.backward()
 

vit/transformer.py
@@ -18,8 +18,7 @@ class Block(nn.Module):
         self.mlp = MLP(dim, hidden_dim, dropout=proj_drop)
 
     def forward(self, x):
-        x = self.norm1(x)
-        x += self.atten(x)
+        x = x + self.atten(self.norm1(x))
         return x + self.mlp(self.norm2(x))
 
 